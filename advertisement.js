057282f50c1d28c423d5be0a27d4bf92a961b22a
bangroyhan <royhan@pasti.in>
2015-02-03 14:27:45 +0700

    Create advertisement.js

---
+++ advertisement.js
@@ -0,0 +1,15 @@
+document.write('<div id="adsense" style="visibility: hidden;display:none;">advertisement</div>');
+document.write('<div id="tester" style="visibility: hidden;display:none;">advertisement</div>');
+document.write('<div id="ad-container" style="visibility: hidden;display:none;">advertisement</div>');
+jQuery(document).ready(function()
+{
+jQuery(document).bind("contextmenu", function(){
+return false;	
+});
+});
+
+
+
+
+
+
